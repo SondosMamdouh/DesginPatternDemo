2c925cfa46f342475aeb8af41605b6d68c750ff0
SondosMamdouh <sondosmamdouh.sm@gmail.com>
2020-10-18 15:15:34 +0200

    first commit

---
+++ AdapterDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDemo
+{
+    //the old interface 
+    public interface ITarget
+    {
+        string GetRequest();
+    }
+
+   // the new one that i want to use 
+    class Adaptee
+    {
+        public string GetSpecificRequest()
+        {
+            return "Specific request.";
+        }
+    }
+
+    //te addpater that will convert implments the old one and contain an object from the new one 
+    class Adapter : ITarget
+    {
+        private readonly Adaptee _adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            this._adaptee = adaptee;
+        }
+
+        public string GetRequest()
+        {
+            return $"This is '{this._adaptee.GetSpecificRequest()}'";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adaptee adaptee = new Adaptee();
+            ITarget target = new Adapter(adaptee);
+
+            Console.WriteLine("Adaptee interface is incompatible with the client.");
+            Console.WriteLine("But with adapter client can call it's method.");
+
+            Console.WriteLine(target.GetRequest());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BridgeDemo/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace BridgeDemo
+{
+    class Abstraction
+
+    {
+        protected Implementor implementor;
+
+        // Property
+
+        public Implementor Implementor
+        {
+            set { implementor = value; }
+        }
+
+        public virtual void Operation()
+        {
+            implementor.Operation();
+        }
+    }
+
+    /// <summary>
+
+    /// The 'Implementor' abstract class
+
+    /// </summary>
+
+    abstract class Implementor
+
+    {
+        public abstract void Operation();
+    }
+
+    /// <summary>
+
+    /// The 'RefinedAbstraction' class
+
+    /// </summary>
+
+    class RefinedAbstraction : Abstraction
+
+    {
+        public override void Operation()
+        {
+            implementor.Operation();
+        }
+    }
+
+    /// <summary>
+
+    /// The 'ConcreteImplementorA' class
+
+    /// </summary>
+
+    class ConcreteImplementorA : Implementor
+
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteImplementorA Operation");
+        }
+    }
+
+    /// <summary>
+
+    /// The 'ConcreteImplementorB' class
+
+    /// </summary>
+
+    class ConcreteImplementorB : Implementor
+
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteImplementorB Operation");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Abstraction ab = new RefinedAbstraction();
+
+            // Set implementation and call
+
+            ab.Implementor = new ConcreteImplementorA();
+            ab.Operation();
+
+            // Change implemention and call
+
+            ab.Implementor = new ConcreteImplementorB();
+            ab.Operation();
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+}
