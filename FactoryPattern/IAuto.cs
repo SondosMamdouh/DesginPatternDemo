bf3912e7fc9607315811f295d87414d8d9c5e6b1
SondosMamdouh <sondosmamdouh.sm@gmail.com>
2020-12-24 15:50:31 +0200

    Factory Pattern Demo

---
+++ FactoryPattern/IAuto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryPattern
+{
+   public interface IAuto
+    {
+        public void TurnOn();
+        public void TurnOff();
+    }
+    public class BMW : IAuto
+    {
+        public void TurnOff()
+        {
+            Console.WriteLine("BMW iS Stopped...");
+        }
+
+        public void TurnOn()
+        {
+            Console.WriteLine("BMW iS Running ...");
+        }
+    }
+    public class MiniCopper : IAuto
+    {
+
+        public void TurnOff()
+        {
+            Console.WriteLine("MiniCopper iS Stopped...");
+        }
+
+        public void TurnOn()
+        {
+            Console.WriteLine("MiniCopper iS Running ...");
+        }
+    }
+
+    public class NullAuto : IAuto
+    {
+        public void TurnOff()
+        {
+            Console.WriteLine("not A car ");
+        }
+
+        public void TurnOn()
+        {
+            Console.WriteLine("not A car ");
+        }
+    }
+}
---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string carName = Console.ReadLine();
+
+            AutoFactory autoFactory = new AutoFactory();
+            IAuto auto = autoFactory.CreateInstance(carName);
+            auto.TurnOn();
+            auto.TurnOff();
+        }
+    }
+}
